9444a9e56f948c44de03a0164e255c20b8fe374c
ffreemt <yucongo+fmt@gmail.com>
2023-08-28 12:14:18 +0800

    Update pyqt5-windows.yml

--- use-pyqt5.py
+++ use-pyqt5.py
@@ -1,18 +1,19 @@
 import sys
 
-from PyQt5.QtWidgets import *
+# from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QGridLayout, QWidget, QApplication
 
 
 class MainUi(QWidget):
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
         self.main_layout = QGridLayout()
         self.setLayout(self.main_layout)
-        
+
         self.start()
-    
+
     def start(self):
         pass
 
@@ -21,12 +22,12 @@ if __name__ == '__main__':
     """
     Main run
     """
-    
+
     app = QApplication(sys.argv)
-    
+
     ui = MainUi()
     ui.show()
-    
+
     sys.exit(app.exec_())
 
 
