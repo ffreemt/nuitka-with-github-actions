79eb6507ccd36a89880025b81120b34957cc0a99
ffreemt <yucongo+fmt@gmail.com>
2023-08-28 12:08:03 +0800

    Update poetry add PyQt5

---
+++ use-pyqt5.py
@@ -0,0 +1,37 @@
+import sys
+
+from PyQt5.QtWidgets import *
+
+
+class MainUi(QWidget):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.main_layout = QGridLayout()
+        self.setLayout(self.main_layout)
+        
+        self.start()
+    
+    def start(self):
+        pass
+
+
+if __name__ == '__main__':
+    """
+    Main run
+    """
+    
+    app = QApplication(sys.argv)
+    
+    ui = MainUi()
+    ui.show()
+    
+    sys.exit(app.exec_())
+
+
+"""
+
+nuitka --mingw64 --standalone --enable-plugin=pyqt5 use_pyqt5.py
+
+"""
